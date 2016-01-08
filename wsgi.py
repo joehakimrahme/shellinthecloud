7d13d19bf162ec9729fb7c4008e1dcb330ac9baf
Joe H. Rahme <jhakimra@redhat.com>
2016-01-08 19:30:42 +0100

    Initial Commit

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+
+from blogstrap import create_app
+
+BASE = os.path.dirname(os.path.abspath(__file__))
+config = lambda x: os.path.join(BASE, x)
+
+application = create_app(config('.blogstrap.conf'))
+
+if __name__ == '__main__':
+    application.run()
