2cf4d9d1fcd996a79864035703fc900cac38e4cb
Joe H. Rahme <jhakimra@redhat.com>
2017-06-13 23:54:56 +0200

    Refactors directory skeleton

--- .blogstrap/wsgi.py
+++ .blogstrap/wsgi.py
@@ -5,7 +5,7 @@ from blogstrap import create_app
 BASE = os.path.dirname(os.path.abspath(__file__))
 config = lambda x: os.path.join(BASE, x)
 
-application = create_app(config('.blogstrap.conf'))
+application = create_app(config('blogstrap.conf'))
 
 if __name__ == '__main__':
     application.run()
